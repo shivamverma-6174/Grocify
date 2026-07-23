eedb8b9c0434acbb0579eedfbd16ec88f818b7da
Shivam Verma <shivamvermamaghar9984@gmail.com>
2026-07-23 22:28:46 +0530

    Add error handling for missing Clerk Publishable Key
    Throw an error if the Clerk Publishable Key is missing.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,6 +10,7 @@ import "../../global.css";
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
 if (!publishableKey) {
+  
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
